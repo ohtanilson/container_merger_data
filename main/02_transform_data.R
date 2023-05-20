d4a60afb59001f6082bd2fccc8db711bf3dc57ce
ohtanilson <suguru.ohtani.n@gmail.com>
2023-05-19 23:59:30 -0500

    Update 02_transform_data.R

--- main/02_transform_data.R
+++ main/02_transform_data.R
@@ -32,6 +32,14 @@ operator_level_entry_exit_merger_HBdata <-
     .keep_all = TRUE
   ) 
 
+operator_level_entry_exit_merger_IHS <-
+  readr::read_csv("input/operator_level_entry_exit_merger_IHS.csv") %>% 
+  dplyr::distinct(
+    parent_company,
+    .keep_all = TRUE
+  ) 
+
+
 # extract data ----
 colnames(HB_data)
 
@@ -160,3 +168,103 @@ operator_panel_data <-
     targeted_shipdetails_container_data,
     targeted_CIY_data
     )
+
+
+
+unique_operator_name_list_HB_and_CIY <-
+  operator_panel_data %>% 
+  dplyr::filter(
+    data_source == "HB" |
+      data_source == "CIY"
+  ) %>%
+  dplyr::arrange(
+    operator,
+    year
+    ) %>% 
+  dplyr::distinct(
+    operator,
+    data_source,
+    .keep_all = TRUE
+  )
+
+unique_operator_name_list_IHS <-
+  operator_panel_data %>% 
+  dplyr::filter(
+    data_source == "IHS"
+  ) %>% 
+  dplyr::mutate(
+    connection_HB_or_CIY =
+      ifelse(year == 1990 | year == 2006,
+             1,
+             0)
+  ) %>%
+  dplyr::arrange(year) %>% 
+  dplyr::distinct(
+    operator,
+    data_source,
+    connection_HB_or_CIY,
+    .keep_all = TRUE
+  ) %>% 
+  dplyr::select(
+    year,
+    data_source,
+    country_or_market,
+    operator,
+    #connection_HB_or_CIY
+  ) %>% 
+  dplyr::arrange(
+    operator
+  ) %>% 
+  dplyr::mutate(
+    operator_rename =
+      stringr::str_to_title(operator)
+  ) %>% 
+  dplyr::left_join(
+    operator_level_entry_exit_merger_IHS,
+    by = c("operator" = "parent_company")
+  ) %>% 
+  dplyr::select(
+    year,
+    data_source:operator_rename,
+    `name in CIY`,
+    `name in HB`,
+    merging_firm#,
+    #connection_HB_or_CIY
+  ) %>% 
+  dplyr::arrange(
+    operator
+  ) %>% 
+  dplyr::mutate(
+    `name in CIY` =
+      ifelse(
+        is.na(`name in CIY`) == 1,
+        ifelse(year == 1990,
+               year,
+               NA),
+        `name in CIY`
+      ),
+    `name in HB` =
+      ifelse(
+        is.na(`name in HB`) == 1,
+        ifelse(year == 2006,
+               year,
+               NA),
+        `name in HB`
+      )
+  )
+unique_merging_firm_name_list <-
+  unique_operator_name_list_IHS %>%
+  dplyr::distinct(
+    merging_firm
+  ) %>%
+  dplyr::mutate(
+    operator_rename =
+      stringr::str_to_title(merging_firm)
+  )
+
+# save ----
+
+write.csv(unique_operator_name_list_HB_and_CIY, 
+          file = "cleaned/unique_operator_name_list_HB_and_CIY.csv")
+write.csv(unique_operator_name_list_IHS, 
+          file = "cleaned/unique_operator_name_list_IHS.csv")
